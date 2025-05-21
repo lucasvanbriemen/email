cdb8f509b6433df2c6fd05d36137b2174e327fcb
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-05-21 16:31:35 +0200

    add date header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,21 @@
 
     @vite('resources/css/email/index.scss')
 
+    @php $last_iterated_date = null; @endphp
     @foreach ($messages as $message)
+        @php
+            $current_iteration_date = date("Y-m-d", strtotime($message['sent_at']));
+        @endphp
+
+        @if ($last_iterated_date != $current_iteration_date)
+            <h2 class='email-date'>{{ date("D, d M Y", strtotime($message['sent_at'])) }}</h2>
+            @php $last_iterated_date = $current_iteration_date; @endphp
+        @endif
+
         <a href='/folder/{{ $selectedFolder }}/mail/{{ $message['uid'] }}' class='message {{ $message['has_read'] ? 'read' : 'unread' }}'>
             <p class='email-from'>{{ $message['from'] }}</p>
             <p class='email-subject'>{{ $message['subject'] }}</p>
-            <p class='email-sent-at'>{{ date("D, d M Y", strtotime($message['sent_at'])) }}</p>
+            <p class='email-sent-at'>{{ date("H:m", strtotime($message['sent_at'])) }}</p>
         </a>
 
     @endforeach
