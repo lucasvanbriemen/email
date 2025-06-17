435c421dbc0f1e8634358fe50fed977cbf51e07b
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-06-17 22:09:52 +0200

    fix formatting

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -3,7 +3,7 @@
     @vite(['resources/css/email/overview.scss', 'resources/js/email/overview.js'])
 
     @php
-$last_iterated_date = null;
+        $last_iterated_date = null;
     @endphp
 
     @if (count($emailThreads) == 0)
@@ -14,51 +14,56 @@ $last_iterated_date = null;
     @endif
 
     @foreach ($emailThreads as $emailThread)
+        @php
+            $current_iteration_date = date('Y-m-d', strtotime($emailThread[0]['sent_at']));
+        @endphp
 
-            @php
-        $current_iteration_date = date("Y-m-d", strtotime($emailThread[0]['sent_at']));
-            @endphp
-
-            @if ($last_iterated_date != $current_iteration_date)
-                <h2 class='email-date'>{{ date("D, d M Y", strtotime($emailThread[0]['sent_at'])) }}</h2>
-                @php $last_iterated_date = $current_iteration_date; @endphp
-            @endif
+        @if ($last_iterated_date != $current_iteration_date)
+            <h2 class='email-date'>{{ date('D, d M Y', strtotime($emailThread[0]['sent_at'])) }}</h2>
+            @php $last_iterated_date = $current_iteration_date; @endphp
+        @endif
 
 
-            @if (count($emailThread) > 1)
-                <?php        $uuid = uniqid('thread-'); ?>
-                <div class='email-thread {{ $uuid }} '>
-                    @include('email_listing', [
-                'email' => $emailThread[0],
-                'class' => 'thead-top-message ' .
-                    (in_array(false, array_column($emailThread, 'has_read')) ? 'unread' : 'read') . ' ' .
-                    ($emailThread[0]['is_starred'] == 1 ? ' starred' : 'unstarred'),
-                'current_iteration_date' => $current_iteration_date,
-                'size' => count($emailThread),
-                'thread' => true,
-                'selectedCredential' => $selectedCredential,
-                'uuid' => $uuid,
-                'is_fully_read' => !in_array(false, array_column($emailThread, 'has_read')),
-            ])
-            @endif
-
-            @foreach ($emailThread as $email)
+        @if (count($emailThread) > 1)
+            <?php $uuid = uniqid('thread-'); ?>
+            <div class='email-thread {{ $uuid }} '>
                 @include('email_listing', [
+                    'email' => $emailThread[0],
+                    'class' =>
+                        'thead-top-message ' .
+                        (in_array(false, array_column($emailThread, 'has_read')) ? 'unread' : 'read') .
+                        ' ' .
+                        ($emailThread[0]['is_starred'] == 1 ? ' starred' : 'unstarred'),
+                    'current_iteration_date' => $current_iteration_date,
+                    'size' => count($emailThread),
+                    'thread' => true,
+                    'selectedCredential' => $selectedCredential,
+                    'uuid' => $uuid,
+                    'is_fully_read' => !in_array(false, array_column($emailThread, 'has_read')),
+                ])
+        @endif
+
+        @foreach ($emailThread as $email)
+            @include('email_listing', [
                 'email' => $email,
-                'class' => 'message ' . ($email['has_read'] ? 'read' : 'unread') . ' ' .
+                'class' =>
+                    'message ' .
+                    ($email['has_read'] ? 'read' : 'unread') .
+                    ' ' .
                     ($email['is_starred'] == 1 ? ' starred' : 'unstarred'),
-                'dataUrl' => '/' . $selectedCredential->id . '/folder/' . $selectedFolder . '/mail/' . $email['uuid'],
+                'dataUrl' =>
+                    '/' . $selectedCredential->id . '/folder/' . $selectedFolder . '/mail/' . $email['uuid'],
                 'current_iteration_date' => $current_iteration_date,
                 'selectedCredential' => $selectedCredential,
                 'thead' => false,
                 'uuid' => uniqid('message-'),
                 'is_fully_read' => !in_array(false, array_column($emailThread, 'has_read')),
             ])
-            @endforeach
+        @endforeach
 
-            @if (count($emailThread) > 1)
-                </div>
-            @endif
+        @if (count($emailThread) > 1)
+            </div>
+        @endif
     @endforeach
 
     @include('context_menu')
