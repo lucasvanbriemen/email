ec4eb328396cbc7963338699647b334845006b1c
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-06-22 11:58:09 +0200

    migrate imap to profile

---
+++ database/migrations/2025_06_22_115517_use_profiles_for_folders.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Profiles;
+use App\Models\Folder;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::table('folders', function (Blueprint $table) {
+            // Add profile_id to folders table
+            $table->foreignId('profile_id')->constrained('profiles')->onDelete('cascade');
+
+            // Remove user_id from folders table
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+
+        // set the profile_id to the profile that has the same username as the imap credential username
+        Folder::all()->each(function ($folder) {
+            $profile = Profiles::where('email', $folder->imap_credential->username)->first();
+
+            if ($profile) {
+                $folder->profile_id = $profile->id;
+                $folder->save();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
