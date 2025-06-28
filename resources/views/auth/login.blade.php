f541c8d7bd3173ff6be8364afc92a94322f324a3
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-06-28 12:05:10 +0200

    revamp login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,18 +10,6 @@
         <x-input type="checkbox" name="remember" id="remember_me" class="remember-me-checkbox" />
 
         <x-input type="submit" class="call-to-action" value="{{ __('Log in') }}" name="Log in" />
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
     </form>
 
     <script>
