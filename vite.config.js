f1f4ae56fb402349d73d38724930291f05fbfbc9
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-07-01 08:36:55 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import fg from 'fast-glob';
 import laravel from 'laravel-vite-plugin';
 
 const files = fg.sync([
-    'resources/js/**/app.js',
+    'resources/js/**/*.js',
     'resources/css/**/*.css',
     'resources/css/**/*.scss',
 ]);
