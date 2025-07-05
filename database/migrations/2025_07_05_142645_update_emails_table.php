4cd193eee1cae83d483c487a806cb47379210d75
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-07-05 14:31:38 +0200

    fix db

---
+++ database/migrations/2025_07_05_142645_update_emails_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('emails', function (Blueprint $table) {
+            // drop the foreign key constraint if it exists
+            if (Schema::hasColumn('emails', 'credential_id')) {
+                $table->dropForeign('emails_user_id_foreign');
+                $table->dropColumn('credential_id');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('emails', function (Blueprint $table) {
+            //
+        });
+    }
+};
