b9812078690e698bc02aa6069429b93b1e7d2833
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-07-25 23:20:46 +0200

    cleanup

--- app/Http/Middleware/IsLoggedIn.php
+++ app/Http/Middleware/IsLoggedIn.php
@@ -15,39 +15,18 @@ class IsLoggedIn
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // Rebuild cookie header from original request
-        $cookieJar = implode('; ', array_map(
-            fn($k, $v) => "$k=$v",
-            array_keys($request->cookies->all()),
-            $request->cookies->all()
-        ));
-
-
-        var_dump('Checking if user is logged in', $cookieJar);
-
         $laravelSession = $_COOKIE['auth_token'] ?? null;
-        var_dump('Laravel session cookie', $laravelSession);
 
         $ch = curl_init('https://login.lucasvanbriemen.nl/api/user/token/' . $laravelSession);
-        curl_setopt_array($ch, [
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HEADER => true,
-        CURLOPT_HTTPHEADER => [
-            'Accept: application/json',
-            'Content-Type: application/json',
-            "Cookie: $laravelSession"
-        ]
-        ]);
-
         $response = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
         if ($httpCode === 200) {
-            dd('User is logged in', $response, $httpCode);
+            dd($response);
             return $next($request);
+        } else {
+            dd('You are not logged in. Please log in to continue.', $response, $httpCode);
         }
-
-        var_dump('You are not logged in. Please log in to continue.', $response, $httpCode);
     }
 }
