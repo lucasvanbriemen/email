b9df252266c5e40c06c2939b707d7232f54f4c56
lucasvanbriemen <vanbriemenlucas@gmail.com>
2025-09-04 23:06:14 +0200

    Refactor email sender avatar display logic in email_listing.blade.php.

--- resources/views/email_listing.blade.php
+++ resources/views/email_listing.blade.php
@@ -37,7 +37,12 @@
 
 <div class='email-item {{ $class }}' data-email-id='{{ $email['uuid'] }}' data-path='{{ $pathToEmail }}' data-context-menu='{{ $contextMenu }}'>
     <div class='email-from'>
-        <img src="/{{ $email->sender->logo_url }}" alt="{{ $email['from'] }}" class='email-avatar'>
+
+        @if ($email->sender == null)
+            {{ dump($email) }} - {{ dump($email->sender_id) }}
+        @else
+            <img src="/{{ $email->sender->logo_url }}" alt="{{ $email['from'] }}" class='email-avatar'>
+        @endif
         {{ $email['from'] }}
     </div>
     <p class='email-subject'>{{ $email['subject'] }}</p>
