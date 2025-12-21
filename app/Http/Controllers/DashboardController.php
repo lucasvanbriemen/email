5db69990ffbe35b19d4eb80a3a85da5ac4556668
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-12-21 21:51:56 +0100

    Link to main user.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,29 +13,9 @@ class DashboardController extends Controller
     {
         $profiles = Profile::where('user_id', currentUser()->id)->get();
 
-        // If there is only one, we dont need to show the profile selection
-        $last_activity = currentUser()->last_activity;
-
-        $emails = Email::whereIn('profile_id', $profiles->pluck('id'))
-            ->where('created_at', '>', $last_activity)
-            ->orderBy('created_at', 'desc')
-            ->get();
-
-        foreach ($emails as $email) {
-            // set the profile_id to the linked_profile_count
-            $email->profile_id = Profile::where('id', $email->profile_id)->first()->linked_profile_count;
-        }
-
-        $last_update = DB::table('system_info')->value('last_email_fetched_at');
-
-        return view(
-            'dashboard',
-            [
-                'profiles' => $profiles,
-                'emails' => $emails,
-                'last_activity' => $last_activity,
-                'last_update' => $last_update
-            ]
-        );
+        return redirect()->route('mailbox.overview', [
+            'linked_profile_id' => $profiles->first()->linked_profile_count,
+            'folder' => 'inbox'
+        ]);
     }
 }
