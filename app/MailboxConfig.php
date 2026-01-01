05d98b6c3976902ab767d9bd64c6782264c4d20f
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-01-01 21:12:22 +0100

    Cleanup.

--- app/MailboxConfig.php
+++ app/MailboxConfig.php
@@ -15,7 +15,7 @@ class MailboxConfig
             'rules' => [
                 'from' => [
                     '*@webinargeek.com'
-                ],
+                ]
             ],
         ],
         [
