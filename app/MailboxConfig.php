dbf5e1098b69c967224bb4b3452235a9180c97a4
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-01-01 21:08:56 +0100

    Create rules for sorting arrays of System.Object.

--- app/MailboxConfig.php
+++ app/MailboxConfig.php
@@ -12,14 +12,30 @@ class MailboxConfig
         [
             'path' => 'work',
             'name' => 'Work',
+            'rules' => [
+                'from' => [
+                    '*@webinargeek.com'
+                ],
+            ],
         ],
         [
             'path' => 'github',
             'name' => 'GitHub',
+            'rules' => [
+                'from' => [
+                    '@github.com',
+                    '@notifications.github.com',
+                ]
+            ],
         ],
         [
             'path' => 'pathe',
             'name' => 'Pathe',
+            'rules' => [
+                'from' => [
+                    '*@service.pathe.nl'
+                ],
+            ],
         ],
     ];
 }
