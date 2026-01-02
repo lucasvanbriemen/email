01851a67eaef5ca48862be047da94ee5fa7f909e
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-01-02 11:29:24 +0100

    Render items using <li> element.

---
+++ resources/js/components/ListItem.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Icon from "./Icon.svelte";
+  let { email } = $props();
+
+  function itemUrl(number) {
+    const base = window.location.href;
+    return `${base}/${number}`;
+  }
+
+  function subTitle() {
+    return `opened ${email.created_at} by ${email.sender_name}`;
+  }
+
+</script>
+
+<a class="list-item" href="{itemUrl(email.uuid)}">
+  <Icon name=logo size="1.5rem"/>
+
+  <div class="content">
+    <h3>{email.subject}</h3>
+    <div class="meta">
+      {subTitle()}
+    </div>
+  </div>
+</a>
+
+<style lang="scss">
+  @import '../../scss/components/list-item.scss';
+</style>
