d49265abd5047d1b65a3e72910e7703b5887d736
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-01-02 13:17:50 +0100

    Set perfect height.

--- resources/js/components/pages/Email.svelte
+++ resources/js/components/pages/Email.svelte
@@ -4,6 +4,8 @@
   let { uuid } = $props();
   let email = $state({});
   let isLoading = $state(true);
+  let iframeEl;
+  let iframeHeight = $state("auto");
 
   onMount(async () => {
     await loadEmail();
@@ -20,7 +22,14 @@
       return { src: email.html_body };
     }
 
-    return { srcDoc: `<style>body{font-family: sans-serif;}</style><base target='_top'>${email.html_body}` };
+    return { srcDoc: `<style>body{font-family: sans-serif; margin: 0; padding: 0;} html{margin: 0; padding: 0;}</style><base target='_top'>${email.html_body}` };
+  }
+
+  function handleIframeLoad() {
+    if (iframeEl?.contentDocument?.body) {
+      const height = iframeEl.contentDocument.body.scrollHeight;
+      iframeHeight = `${height}px`;
+    }
   }
 
   $effect(() => {
@@ -38,7 +47,14 @@
       <p>{email.to} ({email.created_at_human})</p>
     </div>
 
-    <iframe {...getIframeProps(email)} frameborder="0" width="100%"></iframe>
+    <iframe
+      bind:this={iframeEl}
+      {...getIframeProps(email)}
+      frameborder="0"
+      width="100%"
+      style="height: {iframeHeight}; border: none;"
+      onload={handleIframeLoad}
+    ></iframe>
   </article>
 {/if}
 
