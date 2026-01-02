b4c9ecfb687efbdbd1c4d2d0dd427f8e25f47109
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-01-02 12:29:42 +0100

    Render sender.

--- resources/js/components/pages/Email.svelte
+++ resources/js/components/pages/Email.svelte
@@ -3,19 +3,26 @@
 
   let { uuid } = $props();
   let email = $state({});
+  let isLoading = $state(true);
 
   onMount(async () => {
     email = await api.get("/api/email/" + uuid);
+    isLoading = false;
   });
 </script>
 
-<article>
-  <header>
-    <h1>{email.subject}</h1>
-  </header>
+{#if isLoading}
+  <p>Loading email...</p>
+{:else}
+  <article>
+    <header>
+      <h1>{email.subject}</h1>
+      <h1>{email.sender.name}</h1>
+    </header>
 
-  <iframe srcdoc={email.html_body} frameborder="0" width="100%" height="600px"></iframe>
-</article>
+    <iframe srcdoc={email.html_body} frameborder="0" width="100%" height="600px"></iframe>
+  </article>
+{/if}
 
 <style>
   @import '../../../scss/pages/email.scss';
