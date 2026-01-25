57072e41691ec7a11f9ed9802e3d170fb11f19b9
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-01-25 11:00:29 +0100

    Add test route.

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,7 @@ Route::middleware(IsLoggedIn::class)->group(function () {
     Route::get('/mailbox/{group}', [MailboxController::class, 'index'])->name('mailbox.emails');
     Route::get('/email/{uuid}', [MailboxController::class, 'show'])->name('email.view');
 });
+
+Route::get('404', function () {
+    return response()->json(['message' => 'Endpoint not found.'], 404);
+});
