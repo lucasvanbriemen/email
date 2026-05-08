aa15368dafeb2b3235c5167c9c397307ecdc6086
Lucas <contact@lucasvanbriemen.nl>
2026-05-08 12:00:25 +0200

    Add theme colors.

--- resources/js/lib/theme.js
+++ resources/js/lib/theme.js
@@ -3,9 +3,12 @@ import api from "./api.js";
 export default {
   themeUrl: "https://components.lucasvanbriemen.nl/api/colors",
 
-  custom_colors: [
-    
-  ],
+  custom_colors: {
+    "starred": {
+      "dark": "rgb(238, 222, 108)",
+      "light": "rgb(248, 255, 38)"
+    },
+  },
 
   getTheme() {
     const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
@@ -16,17 +19,11 @@ export default {
     document.documentElement.setAttribute("data-theme", this.getTheme());
     const colors = await api.get(this.themeUrl);
 
-    colors.forEach(color => {
-      const name = `--${color.name}`;
-      const value = this.getTheme() === "dark" ? color.dark : color.light;
-      document.documentElement.style.setProperty(name, value);
-    });
-    
+    const mergedColors = { ...colors, ...this.custom_colors };
 
-    this.custom_colors.forEach(color => {
-      const name = `--${color.name}`;
+    Object.entries(mergedColors).forEach(([name, color]) => {
       const value = this.getTheme() === "dark" ? color.dark : color.light;
-      document.documentElement.style.setProperty(name, value);
+      document.documentElement.style.setProperty(`--${name}`, value);
     });
   },
 };
