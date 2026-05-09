1ac4d5a9a51037961cf218bca6ed9e29efd64c55
Lucas <contact@lucasvanbriemen.nl>
2026-05-09 16:31:00 +0200

    Discontinue the agent.

--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\IsLoggedIn;
-use App\Http\Middleware\AgentApiAuth;
 use App\Http\Controllers\MailboxController;
-use App\Http\Controllers\AgentApiController;
 
 Route::middleware(IsLoggedIn::class)->group(function () {
     Route::get('/mailbox/metadata', [MailboxController::class, 'metadata'])->name('mailbox.metadata');
     Route::get('/mailbox/{group}', [MailboxController::class, 'index'])->name('mailbox.emails');
     Route::get('/email/{uuid}', [MailboxController::class, 'show'])->name('email.view');
-});
-
-Route::middleware(AgentApiAuth::class)->group(function () {
-    Route::get('/emails/search', [AgentApiController::class, 'search'])->name('api.emails.search');
-    Route::get('/emails/{id}', [AgentApiController::class, 'show'])->name('api.emails.show');
 });
 
 Route::get('404', function () {
