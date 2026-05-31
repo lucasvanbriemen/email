1daecd1efd4f0cc225e240a12b133ddc621522df
Lucas <contact@lucasvanbriemen.nl>
2026-05-31 16:49:02 +0200

    Render all emails.

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,32 +3,10 @@ class EmailsController < ApplicationController
   end
 
   def index
+    @emails = Email.all
   end
 
   def create
-    account = Account.find_by(email: session_params[:email])
-
-    if !account&.authenticate(session_params[:password])
-      flash.now[:alert] = "Invalid email or password"
-      return render :new, status: :unprocessable_entity
-    end
-
-    token = account.tokens.create!(
-      value: SecureRandom.hex(32),
-      expires_at: Time.current + Token::TOKEN_DURATION
-    )
-
-    cookies[:auth_token] = {
-      value: token.value,
-      expires: token.expires_at,
-      httponly: true,
-      secure: Rails.env.production?,
-      domain: :all
-    }
-
-    path = session_params[:redirect_to].presence || root_path
-
-    redirect_to path.to_s + "?auth_token=#{token.value}", notice: "Logged in successfully", allow_other_host: true
   end
 
   def show
