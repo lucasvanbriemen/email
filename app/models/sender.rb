3af1e02afa3768ee31fe97a61d0128928b5e7f1f
Lucas <contact@lucasvanbriemen.nl>
2026-06-02 22:28:34 +0200

    Add image URL to display sender's logo in email index page.

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -1,3 +1,15 @@
 class Sender < ApplicationRecord
   has_many :emails
+
+  def image_url
+    "https://img.logo.dev/#{top_level_domain}?token=pk_YHpEPFuOTnGDZ6nmBhgIog&retina=true"
+  end
+
+  def top_level_domain
+    domain = email.split("@").last
+    domain_parts = domain.split(".")
+    domain_parts.last(2).join(".")
+
+    domain_parts.last
+  end
 end
