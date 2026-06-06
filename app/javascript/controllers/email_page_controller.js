fc1460d1c09abba4a3f20864598ab8883f637013
Lucas <contact@lucasvanbriemen.nl>
2026-06-06 13:59:57 +0200

    Add active state.

--- app/javascript/controllers/email_page_controller.js
+++ app/javascript/controllers/email_page_controller.js
@@ -4,6 +4,9 @@ export default class extends Controller {
   static targets = ["content"]
 
   select(event) {
+    this.element.querySelector(".active")?.classList.remove("active")
+    event.currentTarget.classList.add("active")
+    
     fetch(`/emails/${event.params.id}`)
       .then(response => response.text())
       .then(html => {
