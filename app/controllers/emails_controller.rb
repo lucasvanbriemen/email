7b479828b12e9d39bfa0efa91f63e12d45f5096f
Lucas <contact@lucasvanbriemen.nl>
2026-06-06 10:32:39 +0200

    Render as partial.

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -12,7 +12,7 @@ class EmailsController < ApplicationController
   def show
     @email = Email.find(params[:id])
 
-    render :show
+    render partial: "show", locals: { email: @email }
   end
 
   private
